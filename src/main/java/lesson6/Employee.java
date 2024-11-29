9908e4115fd875649a08c65fa82d7a2f7e5c3065
Таня <tanyakrokhaleva@gmail.com>
2024-11-30 00:24:09 +0500

    Создан класс с внутренним классом

--- src/main/java/lesson6/Employee.java
+++ src/main/java/lesson6/Employee.java
@@ -1,16 +1,46 @@
 package lesson6;
 
 public class Employee {
-    public String name;
-    public String surname;
-    public String family;
-    public String position;
-    public String email;
-    public String phone;
-    public int salary;
-    public int age;
+    private String name;
+    private String surname;
+    private String family;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+    private int age;
 
+    public String getName() {
+        return name;
+    }
 
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
 
     public Employee(String name, String surname, String family, String position, String email, String phone, int salary, int age) {
         this.name = name;
@@ -25,14 +55,14 @@ public class Employee {
 
 
     public void printEmployee(Employee employee) {
-        System.out.println("Имя: " + employee.name);
-        System.out.println("Фамилия: " + employee.surname);
-        System.out.println("Отчество: " + employee.family);
-        System.out.println("Должность: " + employee.position);
-        System.out.println("E-mail: " + employee.email);
-        System.out.println("Телефон: " + employee.phone);
-        System.out.println("Зарплата: " + employee.salary + " руб.");
-        System.out.println("Возраст: " + employee.age);
+        System.out.println("Имя: " + employee.getName());
+        System.out.println("Фамилия: " + employee.getSurname());
+        System.out.println("Отчество: " + employee.getFamily());
+        System.out.println("Должность: " + employee.getPosition());
+        System.out.println("E-mail: " + employee.getEmail());
+        System.out.println("Телефон: " + employee.getPhone());
+        System.out.println("Зарплата: " + employee.getSalary() + " руб.");
+        System.out.println("Возраст: " + employee.getAge());
     }
 
 
