59e228f8e7a27838b3fdbeddd39d044ea8b97a7d
Таня <tanyakrokhaleva@gmail.com>
2024-12-23 19:26:27 +0500

    Правки в вычисление факториала

--- src/main/java/Lesson7/Factorial.java
+++ src/main/java/Lesson7/Factorial.java
@@ -5,6 +5,9 @@ public class Factorial {
             if (n < 0) {
                 throw new IllegalArgumentException("Факториал отрицательного числа не определен.");
             }
+            else if (n > 20) {
+                throw new IllegalArgumentException("Факториал числа больше 20 не поддерживается для вычисления данной программой");
+            }
             long result = 1;
             for (int i = 1; i <= n; i++) {
                 result *= i;
