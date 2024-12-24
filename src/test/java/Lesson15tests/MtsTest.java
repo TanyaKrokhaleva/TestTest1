a77146ea818e307dfa64fbf4128b123ea621d8f4
Таня <tanyakrokhaleva@gmail.com>
2024-12-24 20:36:36 +0500

    начало

---
+++ src/test/java/Lesson15tests/MtsTest.java
@@ -0,0 +1,18 @@
+package Lesson15tests;
+
+import org.junit.jupiter.*;
+import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.WebDriver.*;
+import org.openqa.selenium.*;
+
+public class MtsTest {
+
+
+    @DisplayName("Проверка отрицательных значений")
+//    @ValueSource(ints = {-1, -10})
+    void testFactorialOfNegativeNumber(int input) {
+
+    }
+
+
+}
