a224cb96fff7c047b53c0b1e5e270ddc2019a7fb
Таня <tanyakrokhaleva@gmail.com>
2024-12-25 22:26:18 +0500

    тесты 2: проверка логотипов

--- src/test/java/Lesson15/MtsTest.java
+++ src/test/java/Lesson15/MtsTest.java
@@ -3,19 +3,18 @@ package Lesson15;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.*;
 import org.junit.jupiter.api.DisplayName;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class MtsTest {
@@ -29,9 +28,14 @@ public class MtsTest {
         driver.get("https://www.mts.by/");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         By cookieAccept = By.xpath("//h3[contains(text(), 'Обработка файлов cookie')]/../../..//button[text() = 'Принять']");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(cookieAccept));
-        wait.until(ExpectedConditions.elementToBeClickable(cookieAccept)).click();
-    }
+
+        if(driver.findElement(cookieAccept).isDisplayed()) {
+
+            driver.findElement(cookieAccept).click();
+
+        }
+        }
+
     @AfterAll
     public static void teardown() {
         if (driver != null) {
@@ -44,22 +48,51 @@ public class MtsTest {
     public void openMtsByUrl() {
         String actualTitle = driver.findElement(By.xpath("//div[@class='pay__wrapper']/h2")).getText();
         assertEquals("Онлайн пополнение\nбез комиссии", actualTitle);
+    }
+
+    @DisplayName("Проверка наличия логотипов платежных систем: Visa")
+    @Test
+    public void visaLogoCheck() {
+        WebElement visaLogo = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение ')]/..//img[@alt='Visa']"));
+        assertTrue(visaLogo.isDisplayed(), "Логотип Visa не отображается на странице");
+
+    }
+
+    @DisplayName("Проверка наличия логотипов платежных систем: Verified by Visa")
+    @Test
+    public void verifiedByVisaLogoCheck() {
+        WebElement visaLogo = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение ')]/..//img[@alt='Verified By Visa']"));
+        assertTrue(visaLogo.isDisplayed(), "Логотип Verified by Visa не отображается на странице");
+
+    }
+
+    @DisplayName("Проверка наличия логотипов платежных систем: MasterCard")
+    @Test
+    public void MasterCardLogoCheck() {
+        WebElement masterCardLogo = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение ')]/..//img[@alt='MasterCard']"));
+        assertTrue(masterCardLogo.isDisplayed(), "Логотип MasterCard не отображается на странице");
+
+    }
+
+    @DisplayName("Проверка наличия логотипов платежных систем: MasterCard Secure Code")
+    @Test
+    public void MasterCardSecureCodeLogoCheck() {
+        WebElement MasterCardSecureCodeLogo = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение ')]/..//img[@alt='MasterCard Secure Code']"));
+        assertTrue(MasterCardSecureCodeLogo.isDisplayed(), "Логотип MasterCard Secure Code не отображается на странице");
+
+
+    }
+
+    @DisplayName("Проверка наличия логотипов платежных систем: Белкарт")
+    @Test
+    public void belCardLogoCheck() {
+        WebElement belCardLogo = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение ')]/..//img[@alt='Белкарт']"));
+        assertTrue(belCardLogo.isDisplayed(), "Логотип Белкарт не отображается на странице");
 
     }
 
 
-
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
